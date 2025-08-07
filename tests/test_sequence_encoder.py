2100c2a35a8d39122ad524dbddd3b6b84e2da8f6
zeinab-sheikhi <zeinab.sheykhie@gmail.com>
2025-08-07 16:15:19 +0200

    added test for StandardizedConv1D layer

--- tests/test_sequence_encoder.py
+++ tests/test_sequence_encoder.py
@@ -1,7 +1,7 @@
 
 import torch
 
-from alphagenome.sequence_encoder import RMSBatchNorm1D
+from alphagenome.sequence_encoder import RMSBatchNorm1D, StandardizedConv1D
 
 
 class TestRMSBatchNorm1D:
@@ -18,3 +18,31 @@ class TestRMSBatchNorm1D:
         output = rms_norm(x)
 
         assert output.shape == x.shape
+
+
+class TestStandardizedConv1D:
+    def setup_method(self):
+        self.batch_size = 4
+        self.seq_len = 100
+        self.in_channels = 64
+        self.out_channels = 128
+        self.kernel_size = 5
+        self.eps = 1e-6
+    
+    def test_basic_forward(self):
+        conv = StandardizedConv1D(
+            in_channels=self.in_channels, 
+            out_channels=self.out_channels, 
+            kernel_size=self.kernel_size,
+        )
+
+        x = torch.randn(self.batch_size, self.seq_len, self.in_channels)
+        output = conv(x)
+
+        expected_output_shape = (self.batch_size, self.seq_len, self.out_channels)
+        assert output.shape == expected_output_shape
+    
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__, "-v"])
